cc648da4e478d4b1fdc5bc4b43df2d4eeb709df4
mike881205 <mike881205@gmail.com>
2020-02-23 10:57:57 -0500

    hard coded html for assessment answers

--- client/src/components/Questions/index.js
+++ client/src/components/Questions/index.js
@@ -2,12 +2,28 @@ import React from "react";
 
 class Questions extends React.Component {
     render() {
-       return (
-          <div>
-             <p>The content text!!!</p>
-          </div>
-       );
+        return (
+            <div className="row">
+                <div className="col-sm">
+                    <p>Are shrubs, bushes and grass trimmed back or removed so they do not infringe on or obstruct the walkway (potential tripping hazard)?</p>
+                </div>
+                <div className="col-sm dropdown">
+                    <select className="form-control">
+                    <option defaultValue="" disabled selected>Response</option>
+                        <option>Yes</option>
+                        <option>No</option>
+                        <option>N/A</option>
+                    </select>
+                </div>
+                <div className="col-sm form-group">
+                    <input type="observation" className="form-control" id="observation" placeholder="Observation"></input>
+                </div>
+                <div className="col-sm form-group">
+                    <input type="comment" className="form-control" id="comment" placeholder="Comment"></input>
+                </div>
+            </div>
+        );
     }
- }
+}
 
- export default Questions
+export default Questions
--- client/src/components/Section/index.js
+++ client/src/components/Section/index.js
@@ -3,14 +3,26 @@ import Questions from "../../components/Questions"
 
 class Section extends React.Component {
     render() {
-        const {section, questions, applicable} = this.props.section;
-       return (
-          <div>
-             <h1> {section} </h1>
-            <Questions />
-          </div>
-       );
+        const { section } = this.props.section;
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-sm-3">
+                        <h3> {section} </h3>
+                    </div>
+                    <div className="col-sm-3">
+                        <div className="row">
+                            <input type="checkbox" label="Check me out"></input>
+                            <p>N/A</p>
+                        </div>
+                    </div>
+                    <div className="col-sm-6">
+                    </div>
+                </div>
+                <Questions />
+            </div>
+        );
     }
- }
+}
 
- export default Section
+export default Section
