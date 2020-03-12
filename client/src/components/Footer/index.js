5bb59f523395c2a8be1d9fd1261f3c76832992f1
mike881205 <mike881205@gmail.com>
2020-03-11 23:47:55 -0400

    added mailto link in footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,7 +10,9 @@ class Footer extends Component {
                         <h5>888-607-4650</h5>
                     </div>
                     <div className="col-sm">
-                        <h5>wegcaps@att.net</h5>
+                        <a className="email" href="mailto:wegcaps@att.net">
+                            <h5>wegcaps@att.net</h5>
+                        </a>
                     </div>
                 </div>
             </div>
